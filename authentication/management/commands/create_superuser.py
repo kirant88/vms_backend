d6107eb35c121121cfda578ee42df78992c9a376
kiran <kiran.tondchore@indi4.io>
2026-04-12 10:37:23 +0530

    Trigger deployment.

--- authentication/management/commands/create_superuser.py
+++ authentication/management/commands/create_superuser.py
@@ -4,23 +4,22 @@ from django.db import transaction
 
 User = get_user_model()
 
+
 class Command(BaseCommand):
-    help = 'Create default superuser if it does not exist'
+    help = "Create default superuser if it does not exist"
 
     def handle(self, *args, **options):
         if not User.objects.filter(is_superuser=True).exists():
             with transaction.atomic():
                 user = User.objects.create_superuser(
-                    username='KiranT@1234',
-                    email='kiran.tondchore@indi4.io',
-                    password='KiranT@1234',
-                    first_name='Kiran',
-                    last_name='Tondchore'
+                    username="KiranT@1234",
+                    email="kiran.tondchore@indi4.io",
+                    password="KiranT@1234",
+                    first_name="Kiran",
+                    last_name="Tondchore",
                 )
                 self.stdout.write(
-                    self.style.SUCCESS(f'Superuser created: {user.email}')
+                    self.style.SUCCESS(f"Superuser created: {user.email}")
                 )
         else:
-            self.stdout.write(
-                self.style.WARNING('Superuser already exists')
-            )
+            self.stdout.write(self.style.WARNING("Superuser already exists"))
