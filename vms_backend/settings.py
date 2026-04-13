6191bd4c1f5b04cb4b6e247e53df11a287af2874
kiran <kiran.tondchore@indi4.io>
2026-04-13 10:06:13 +0530

    Syntax error

--- vms_backend/settings.py
+++ vms_backend/settings.py
@@ -137,8 +137,7 @@ else:
     # Production - Specific origins only
     CORS_ALLOWED_ORIGINS = os.environ.get(
         "CORS_ALLOWED_ORIGINS",
-        "http://localhost:5173",
-        "https://visitor-management-system-amber.vercel.app/",
+        "http://localhost:5173,https://visitor-management-system-amber.vercel.app",
     ).split(",")
 
 CORS_ALLOW_CREDENTIALS = True
